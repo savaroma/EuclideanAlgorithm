10d90f85756a99109525299f7afb43714e14c81a
Aazz <aazz@i.ua>
2016-02-02 01:58:33 +0200

    DoubleZerosException -> Туалетное исключение

--- src/com/switchup/euclidean_int/CommonDivisorNotFoundException.java
+++ src/com/switchup/euclidean_int/CommonDivisorNotFoundException.java
@@ -1,9 +1,7 @@
 package com.switchup.euclidean_int;
 
 public class CommonDivisorNotFoundException extends Exception {
-    private final String message = "[FAIL]: "; //по-моему излишне выносить в поле класса это одно слово. И идея так считает.
-
     public String getMessage(int a, int b) {
-        return message + "Integers " + a + " and " + b + " have common divisor equals \"1\"!";
+        return "[FAIL]: Integers " + a + " and " + b + " have common divisor equals \"1\"!";
     }
 }
