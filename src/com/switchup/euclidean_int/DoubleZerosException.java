15ab751cb77d46cbf46069c1dc028098d6aa626a
Aazz <aazz@i.ua>
2016-02-02 01:56:17 +0200

    DoubleZerosException -> Туалетное исключение

--- src/com/switchup/euclidean_int/DoubleZerosException.java
+++ src/com/switchup/euclidean_int/DoubleZerosException.java
@@ -1,6 +1,6 @@
 package com.switchup.euclidean_int;
 
-public class DoubleZerosException extends Exception {
+public class DoubleZerosException extends Exception {//Туалетное исключение :)
     public String getMessage() {
         return "Greatest common divisor of two zeros equals infinity!";
     }
