66d2aa4efbec292e05a3d87eea8800d69ff14dc4
Aazz <aazz@i.ua>
2016-02-02 01:26:15 +0200

    Наибольший общий делитель у любого числа и единицы, будет равен единице

--- src/com/switchup/euclidean_int/CommonDivisorNotFoundException.java
+++ src/com/switchup/euclidean_int/CommonDivisorNotFoundException.java
@@ -4,6 +4,6 @@ public class CommonDivisorNotFoundException extends Exception {
     private final String message = "[FAIL]: "; //по-моему излишне выносить в поле класса это одно слово. И идея так считает.
 
     public String getMessage(int a, int b) {
-        return message + "Integers " + a + " and " + b + " don't have common divisor";
+        return message + "Integers " + a + " and " + b + " have common divisor equals \"1\"!";
     }
 }
