d3520a6877bd0914c065d394d1bfd15d4ee3e62d
Vyacheslav Kruhlov <v.kruglov69@gmail.com>
2016-02-02 12:48:01 +0200

    corrected code, deleted CommonDivisorNotFoundException

--- src/com/switchup/euclidean_int/CommonDivisorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.switchup.euclidean_int;
-
-public class CommonDivisorNotFoundException extends Exception {
-    public String getMessage(int a, int b) {
-        return "[FAIL]: Integers " + a + " and " + b + " have common divisor equals \"1\"!";
-    }
-}
